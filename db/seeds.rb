84f7a9b207869c930eeae0e0af36a27598b8082e
plasante <pierre.lasante@videotron.ca>
2014-07-12 07:43:48 -0700

    J'ai ajouter du cafe et de la margarine

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,11 +41,14 @@ Category.create(name: 'Produce')
 category_bakery         = Category.find_by(name: 'bakery')
 category_dairy_products = Category.find_by(name: 'dairy products')
 category_beverages      = Category.find_by(name: 'beverages')
+category_coffees        = Category.find_by(name: 'coffee tea & cocoa')
 
 SubCategory.delete_all
 category_bakery.sub_categories.build(name: 'bread').save
 category_dairy_products.sub_categories.build(name: 'milk').save
+category_dairy_products.sub_categories.build(name: 'margarine').save
 category_beverages.sub_categories.build(name: 'soda').save
+category_coffees.sub_categories.build(name: 'coffee').save
 
 SubSubCategory.delete_all
 category_bakery_bread = SubCategory.find_by(name: 'bread')
@@ -58,46 +61,66 @@ category_beverages_soda = SubCategory.find_by(name: 'soda')
 category_beverages_soda.sub_sub_categories.build(name: 'cola').save
 
 Brand.delete_all
-brand_bakery_bread = Brand.create(name: 'villagio')
+brand_bakery_bread = Brand.create(name: 'weston')
 brand_dairy_products_milk = Brand.create(name: 'lactantia')
+brand_dairy_products_margarine = Brand.create(name: 'fleishman')
 brand_beverages_soda = Brand.create(name: 'pepsi')
+brand_coffee = Brand.create(name: 'tassimo')
 
 Product.delete_all
 bread_category = SubSubCategory.find_by(name: 'sliced')
 milk_category  = SubSubCategory.find_by(name: 'reduced fat')
 soda_category  = SubSubCategory.find_by(name: 'cola')
+margarine_category = SubCategory.find_by(name: 'margarine')
+coffee_category = SubCategory.find_by(name: 'coffee')
 
 bread_product = Product.create(brand: brand_bakery_bread,        
                      categorizable: bread_category, 
-                     description: 'Villagio sliced bread')
+                     description: 'Weston white sliced bread')
 milk_product  = Product.create(brand: brand_dairy_products_milk, 
                      categorizable: milk_category,  
-                     description: 'Lactantia 2% milk')
+                     description: 'Lactantia milk 2%')
+margarine_product  = Product.create(brand: brand_dairy_products_margarine, 
+                     categorizable: margarine_category,  
+                     description: 'Fleishman margarine 907g')
 soda_product  = Product.create(brand: brand_beverages_soda,      
                      categorizable: soda_category,  
-                     description: 'Pepsi cola x 24')
+                     description: 'Pepsi cola 24 x 355ml')
+coffee_product = Product.create(brand: brand_coffee,
+                     categorizable: coffee_category,  
+                     description: 'Tassimo Nabob breakfast blend ')
 
 RetailersProduct.delete_all          
 
-bread_product.retailers_products.build(retailer: maxi, price: 4.89).save
+bread_product.retailers_products.build(retailer: maxi, price: 4.39).save
 milk_product.retailers_products.build(retailer:  maxi, price: 5.79).save
 soda_product.retailers_products.build(retailer:  maxi, price: 6.99).save
+margarine_product.retailers_products.build(retailer: maxi, price: 7.49).save
+coffee_product.retailers_products.build(retailer: maxi, price: 9.49).save
 
-bread_product.retailers_products.build(retailer: iga,  price: 4.79).save
+bread_product.retailers_products.build(retailer: iga,  price: 4.29).save
 milk_product.retailers_products.build(retailer:  iga,  price: 5.69).save
 soda_product.retailers_products.build(retailer:  iga,  price: 6.89).save
+margarine_product.retailers_products.build(retailer: iga, price: 7.39).save
+coffee_product.retailers_products.build(retailer: iga, price: 9.39).save
 
-bread_product.retailers_products.build(retailer: metro,  price: 4.69).save
+bread_product.retailers_products.build(retailer: metro,  price: 4.19).save
 milk_product.retailers_products.build(retailer:  metro,  price: 5.59).save
 soda_product.retailers_products.build(retailer:  metro,  price: 6.79).save
+margarine_product.retailers_products.build(retailer: metro, price: 7.29).save
+coffee_product.retailers_products.build(retailer: metro, price: 9.29).save
 
 bread_product.retailers_products.build(retailer: superc,  price: 999999.01).save
 milk_product.retailers_products.build(retailer:  superc,  price: 999999.01).save
 soda_product.retailers_products.build(retailer:  superc,  price: 999999.01).save
+margarine_product.retailers_products.build(retailer: superc, price: 999999.01).save
+coffee_product.retailers_products.build(retailer: superc, price: 999999.01).save
 
 bread_product.retailers_products.build(retailer: tiger,  price: 999999.01).save
 milk_product.retailers_products.build(retailer:  tiger,  price: 999999.01).save
 soda_product.retailers_products.build(retailer:  tiger,  price: 999999.01).save
+margarine_product.retailers_products.build(retailer: tiger, price: 999999.01).save
+coffee_product.retailers_products.build(retailer: tiger, price: 999999.01).save
 
 GroceryList.delete_all
 grocery_list1 = GroceryList.create(user_id: user1.id)
@@ -107,7 +130,11 @@ GroceryListItem.delete_all
 GroceryListItem.create(grocery_list_id: grocery_list1.id, product_id: bread_product.id)
 GroceryListItem.create(grocery_list_id: grocery_list1.id, product_id: milk_product.id)
 GroceryListItem.create(grocery_list_id: grocery_list1.id, product_id: soda_product.id)
+GroceryListItem.create(grocery_list_id: grocery_list1.id, product_id: margarine_product.id)
+GroceryListItem.create(grocery_list_id: grocery_list1.id, product_id: coffee_product.id)
 
 GroceryListItem.create(grocery_list_id: grocery_list2.id, product_id: bread_product.id)
 GroceryListItem.create(grocery_list_id: grocery_list2.id, product_id: milk_product.id)
 GroceryListItem.create(grocery_list_id: grocery_list2.id, product_id: soda_product.id)
+GroceryListItem.create(grocery_list_id: grocery_list1.id, product_id: margarine_product.id)
+GroceryListItem.create(grocery_list_id: grocery_list1.id, product_id: coffee_product.id)
